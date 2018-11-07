356809dca82bb2fc85c0976833999a92898be59f
hr <17771853317@163.com>
2018-11-07 16:32:55 +0800

    发表页面bug修改 以及优化

--- control/user.js
+++ control/user.js
@@ -2,32 +2,34 @@ const moment = require("moment")
 const conn = require("../db/index.js")
 const register = (req, res) => {
     const body = req.body
-
     // 判断数据是否输入
     console.log(body)
     if (body.username.trim().length <= 0 || body.password.trim().length <= 0 || body.nickname.trim().length <= 0) {
-        return res.status(501).send({ status: 501, msg: "重新输入信息" })
+        return res.send({ status: 501, msg: "重新输入信息" })
     }
     // 判断信息是否重复
     const sql = "select count(*) as c from users where username = ?"
     conn.query(sql, body.username, (err, result) => {
-        if (err) { return res.send({ msg: "请重新输入" }) }
-        if (result[0].c != 0) return res.status(505).send({ msg: "请更换其他用户名重新注册", status: 505 })
+            if (err) { return res.send({ msg: "请重新输入" }) }
+            if (result[0].c != 0) return res.status(505).send({ msg: "请更换其他用户名重新注册", status: 505 })
         body.ctime = moment().format("YYYY-MM-DD HH:mm:ss")
         const inSql = "insert into users set ?"
         conn.query(inSql, body, (err, result) => {
-            if (err) return res.status(504).send({ msg: "注册失败", status: 504 })
-            if (result.affectedRows !== 1) return res.status(505).send({ msg: "注册失败", status: 505 })
+            if (err) return res.send({ msg: "注册失败", status: 504 })
+            if (result.affectedRows !== 1) return res.send({ msg: "注册失败", status: 505 })
             res.send({ status: 200, msg: "ok" })
         })
     })
 }
 const login = (req, res) => {
     const body = req.body
+    console.log(body)
+    if(!body.username) {return res.send({msg:"请正确输入用户名",status:404})}
+    if(!body.password) {return res.send({msg:"请输入正确密码",status:404})}
     const sql = "select * from users where  username = ? and password = ?  "
     conn.query(sql, [body.username, body.password], (err, result) => {
         if (err) return res.status(501).send({ msg: "请重新输入" })
-        if (result.length !== 1) return res.status(505).send({ msg: "请更换其他用户名重新注册", status: 505 })
+        if (result.length !== 1) return res.send({ msg: "请重新登录", status: 505 })
         req.session.islogin = true
         req.session.user = result[0]
         let hour = 1000 * 60*60*24*30
--- router/article.js
+++ router/article.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const str = require("../control/article.js")
 router.get('/add/article',str.handleArticleGet)
 router.post('/add/article',str.handleArticlePost)
-router.get('/info/article',str.handleArticleInfoGet)
+router.get('/info/article/:id',str.handleArticleInfoGet)
 module.exports = router
